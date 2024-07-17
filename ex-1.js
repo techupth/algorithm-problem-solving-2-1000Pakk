aa0d34b7faa6ba2bd69f5a81d7f6d5611ab43741
Panpak Apesariyayo <ingpanpak@gmail.com>
2024-07-17 14:17:31 +0700

    Algor

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,26 @@
 function findStudentIndex(students, searchStudent) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  for (let i in students) {
+    if (students[i] === searchStudent) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 // ตอบคำถามตรงนี้จ้า
+students = [
+  "Andrew",
+  "Bobby",
+  "Diana",
+  "George",
+  "Hannah",
+  "Isaac",
+  "Jasmine",
+  "John",
+];
+searchStudent = "John";
+
+let result = findStudentIndex(students, searchStudent);
+
+console.log(result);
--- ex-2.js
+++ ex-2.js
@@ -1,3 +1,26 @@
 function findBookIndex(books, searchTitle) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
+  for (let i in books) {
+    if (books[i].title === searchTitle) {
+      return i;
+    }
+  }
+  return -1;
 }
+
+// ตอบคำถามตรงนี้จ้า
+books = [
+  { title: "A Tale of Two Cities", author: "Charles Dickens" },
+  { title: "Brave New World", author: "Aldous Huxley" },
+  { title: "Catch-22", author: "Joseph Heller" },
+  { title: "Don Quixote", author: "Miguel de Cervantes" },
+  { title: "Ender's Game", author: "Orson Scott Card" },
+  { title: "Fahrenheit 451", author: "Ray Bradbury" },
+  { title: "Gone with the Wind", author: "Margaret Mitchell" },
+  { title: "Harry Potter", author: "J.K. Rowling" },
+];
+searchTitle = "Gone with the Wind";
+
+let result = findBookIndex(books, searchTitle);
+
+console.log(result);
